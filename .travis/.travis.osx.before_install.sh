7d4eff3a36347df89d915e181fa057abfa510308
Rainer M. Krug <R.M.Krug@gmail.com>
2015-08-27 13:39:10 +0200

    Add osx testing with homebrew
    1) moving the sections of the .travis.yml file into scripts:
    
         - before_install :: .travis.$TRAVIS_OS_NAME.before_install.sh
         - install        :: .travis.$TRAVIS_OS_NAME.install.sh
         - script         :: .travis.$TRAVIS_OS_NAME.script.sh
    
    2) using a travis multiple OS [http://docs.travis-ci.com/user/multi-os/]
       which sets the variable $TRAVIS_OS_NAME to "osx" or "linux" (hopefully
       "windows" in the future as well)
    
       So when testing on linux, the scripts with *.linux.*.sh are called, when
       testing osx the ons with *.osx.*.sh get called
    
    3) adding the different compiler to the matrix
    
    So I end up with three separate test scenarios:
    
    ,----
    | - os: linux
    |   language: c
    |   compiler: gcc
    |
    | - os: linux
    |   language: c
    |   compiler: clang
    |
    | - os: osx
    |   compiler: objective-c
    `----
    
    Dummy for travis
    
    Add .gitignore and Authors
    
    * add commented out section for mailing on fail to .travis.yml
    
    Move sudo: require into matrix cases
    
    revert last change
    
    Remove "complier: ..." from osx
    
    Fix wrong / missing language in osx
    
    * was compiler: objective-c, changed to language: objective-c
    
    Rearrange matrix
    
    Use own defined $OS instead of $TRAVIS_OS_NAME
    
    * $TRAVIS_OS_NAME aseems not to be set correctly. Therefore I use own
      defined selector ($OS)
    
    Fix typo for OS=osx
    
    Move .travis scripts into ./.travis directory
    
    * and according changes in .travis.yml
    
    Test Commit for travis
    
    Revert to using $TRAVIS_OS_NAME
    
    * Multiple OS support now properly supported

---
+++ .travis/.travis.osx.before_install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+# Author: Rainer M. Krug, Rainer@krugs.de
+
+## add taps for homebrew installs
+brew tap rkrug/experimental
+brew tap rkrug/head-only
+brew tap rkrug/dev-only
+brew tap homebrew/science
+brew update
---
+++ .travis/.travis.osx.script.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+# Author: Rainer M. Krug, Rainer@krugs.de
+
+## some tests for the recipe 
+brew audit -v $RECIPE
+
+## install GRASS 7.1 
+brew install --HEAD \
+             --with-nc_spm_08_grass7 \
+             --with-ffmpeg \
+             --with-mysql \
+             --with-netcdf \
+             --with-postgresql \
+             --with-openblas \
+             --with-liblas \
+             grass-71
+
+brew test -v grass-71
+brew info grass-71
+
+## run tests
+## grass71 ./nc_basic_spm_grass7/PERMANENT --exec python -m grass.gunittest.main --location './nc_basic_spm_grass7' --location-type nc
+
+## uninstall grass-71
+brew uninstall grass-71
