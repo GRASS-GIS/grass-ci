GRASS-GIS/grass-ci

commit 85054adafce6bec4eb6fb7676eb4e815ff9028cc

broken links to source code and history in add-ons user manuals (see #3224)
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@70514 15284696-431f-4ddb-bdfa-cd5b030d7da7

scripts/g.extension/g.extension.py
@@ -1170,7 +1170,8 @@ def install_extension_std_platforms(name, source, url):
         'SCRIPTDIR=%s' % dirs['script'],
         'STRINGDIR=%s' % dirs['string'],
         'ETC=%s' % os.path.join(dirs['etc']),
-        'SOURCE_URL=%s' % source_url
+        'SOURCE_URL=%s' % source_url,
+        'BASEDIR=%s' % TMPDIR,
     ]
 
     install_cmd = [

tools/mkhtml.py
@@ -317,7 +317,7 @@ curdir = os.path.abspath(os.path.curdir)
 pgmdir = curdir.replace(topdir, '').lstrip('/')
 if os.getenv('SOURCE_URL', ''):
     # addons
-    pgmname = os.path.basename(pgmdir)
+    pgmname = os.getcwd()[len(os.environ['BASEDIR'])+1:]
     classname = index_names[pgmname[:pgmname.find('.')]]
     url_source = urlparse.urljoin('{0}{1}/'.format(os.environ['SOURCE_URL'], classname),
                                   pgmname)

commit 40c2a383e0cb140f89147efbcb36954a0fbe86df

broken links to source code and history in add-ons user manuals (see #3224)        (wingrass addons builds broken by r70514)
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@70535 15284696-431f-4ddb-bdfa-cd5b030d7da7

scripts/g.extension/g.extension.py
@@ -1170,8 +1170,7 @@ def install_extension_std_platforms(name, source, url):
         'SCRIPTDIR=%s' % dirs['script'],
         'STRINGDIR=%s' % dirs['string'],
         'ETC=%s' % os.path.join(dirs['etc']),
-        'SOURCE_URL=%s' % source_url,
-        'BASEDIR=%s' % TMPDIR,
+        'SOURCE_URL=%s' % source_url
     ]
 
     install_cmd = [

tools/mkhtml.py
@@ -316,8 +316,10 @@ topdir = os.path.abspath(os.getenv("MODULE_TOPDIR"))
 curdir = os.path.abspath(os.path.curdir)
 pgmdir = curdir.replace(topdir, '').lstrip('/')
 if os.getenv('SOURCE_URL', ''):
+    import tempfile
     # addons
-    pgmname = os.getcwd()[len(os.environ['BASEDIR'])+1:]
+    basename = os.getcwd()[len(tempfile.gettempdir())+1:]
+    pgmname = basename[basename.find(os.path.sep)+1:]
     classname = index_names[pgmname[:pgmname.find('.')]]
     url_source = urlparse.urljoin('{0}{1}/'.format(os.environ['SOURCE_URL'], classname),
                                   pgmname)