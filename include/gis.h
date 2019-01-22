GRASS-GIS/grass-ci

commit e0e02c99d1a6aa495adade838dfb405fbe08369d

Introduce warn_unused_result macro
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@73982 15284696-431f-4ddb-bdfa-cd5b030d7da7

include/gis.h
@@ -67,6 +67,13 @@ static const char *GRASS_copyright __attribute__ ((unused))
 #  define HOST_NEWLINE "\n"
 #endif
 
+/*! \brief Generate warning if function return value is unused */
+#if __GNUC_PREREQ (3,4)
+    #define WARN_UNUSED_RESULT __attribute__ ((warn_unused_result))
+#else
+    #define WARN_UNUSED_RESULT
+#endif
+
 /*!
   \brief List of units
 */

include/defs/imagery.h
@@ -164,7 +164,7 @@ int I_InitSigSet(struct SigSet *);
 int I_SigSetNBands(struct SigSet *, int);
 struct ClassSig *I_NewClassSig(struct SigSet *);
 struct SubSig *I_NewSubSig(struct SigSet *, struct ClassSig *);
-int I_ReadSigSet(FILE *, struct SigSet *);
+int I_ReadSigSet(FILE *, struct SigSet *) WARN_UNUSED_RESULT;
 int I_SetSigTitle(struct SigSet *, const char *);
 const char *I_GetSigTitle(const struct SigSet *);
 int I_SetClassTitle(struct ClassSig *, const char *);

commit 8dac354968702d596b452822088a973ca7c2640e

fix compilation error on Windows caused by r73982
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@73998 15284696-431f-4ddb-bdfa-cd5b030d7da7

include/gis.h
@@ -68,10 +68,10 @@ static const char *GRASS_copyright __attribute__ ((unused))
 #endif
 
 /*! \brief Generate warning if function return value is unused */
-#if __GNUC_PREREQ (3,4)
-    #define WARN_UNUSED_RESULT __attribute__ ((warn_unused_result))
+#if __GNUC__ && (__GNUC__ >= 3 && __GNUC_MINOR__ >= 4)
+#   define WARN_UNUSED_RESULT __attribute__ ((warn_unused_result))
 #else
-    #define WARN_UNUSED_RESULT
+#   define WARN_UNUSED_RESULT
 #endif
 
 /*!